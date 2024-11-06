a78df627e81d5a08c786fa06e48f073bd94f96d1
Suzy Kim <skme1103@naver.com>
2024-11-06 19:16:12 +0900

    Install react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./page/Home";
+import Login from "./page/Login";
+import SignUp from "./page/SignUp";
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
     </BrowserRouter>
