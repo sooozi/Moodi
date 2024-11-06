0076e49263ee91fe32027c0075840255a35585b7
Suzy Kim <skme1103@naver.com>
2024-11-06 18:53:50 +0900

    Add page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./page/Home";
 
 function App() {
   return (
@@ -7,6 +9,11 @@ function App() {
       <header className="App-header">
         <p>soozi!</p>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
