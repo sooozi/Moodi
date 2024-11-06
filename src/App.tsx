da95e6c2f429b9ef2870358a1de174dc0729696c
Suzy Kim <skme1103@naver.com>
2024-11-06 19:09:58 +0900

    Add Home

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,8 @@ import Home from "./page/Home";
 function App() {
   return (
     <BrowserRouter>
-      {" "}
-      {/* BrowserRouter로 감싸기 */}
       <div className="App">
-        <header className="App-header">
-          <p>soozi!</p>
-        </header>
         <Routes>
-          {" "}
-          {/* Routes로 Route들을 감싸기 */}
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
