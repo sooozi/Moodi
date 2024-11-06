24ac8be1b5f1521955a99ae06204e197be4ee7a4
Suzy Kim <skme1103@naver.com>
2024-11-06 18:45:09 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hi!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>soozi!</p>
       </header>
     </div>
   );
